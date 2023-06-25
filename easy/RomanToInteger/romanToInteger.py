29a1f40fb435389d2d44214e5efb2ac101e391b5
Zakaria Moussammi <zakariamoussammi87@gmail.com>
2023-06-25 19:12:25 +0100

    longestCommonPrefix Sun Jun 25 19:12:25 +01 2023

--- easy/RomanToInteger/romanToInteger.py
+++ easy/RomanToInteger/romanToInteger.py
@@ -9,7 +9,7 @@ class Solution:
             "D": 500,
             "M": 1000
         }
-        result = 0;
+        result = 0
         l = len(s)
         for i in range(l):
             if i + 1 < l and roman[s[i]] < roman[s[i + 1]]: 
