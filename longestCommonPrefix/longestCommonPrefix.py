933927010e181fef395c979d9f3b96d059fe73cc
zmoussam <zakariamoussammi87@gmail.com>
2023-10-15 17:52:07 +0100

    Sun Oct 15 17:52:07 +01 2023

--- longestCommonPrefix/longestCommonPrefix.py
+++
@@ -1,8 +0,0 @@
-def longestCommonPrefix(strs) -> str:
-    strs.sort()
-    i = 0
-    while i < min(len(strs[0]), len(strs[-1])) and strs[0][i] == strs[-1][i]:
-        i += 1
-    return strs[0][0:i]
-
-print(longestCommonPrefix(["flower","flow","flight"]))
