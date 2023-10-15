f0356ea44fe1dbf9845ae540fe777e4f91d33068
Zakaria moussammi <90983110+zmoussam@users.noreply.github.com>
2023-10-15 17:38:19 +0100

    Time: 28 ms (63.45%), Space: 13.3 MB (29.85%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        roman = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+        result = 0
+        l = len(s)
+        for i in range(l):
+            if i + 1 < l and roman[s[i]] < roman[s[i + 1]]: 
+                result -= roman[s[i]]
+            else :
+                result += roman[s[i]]
+        return result
